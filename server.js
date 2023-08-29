1ef02094e5611b3ac7d4541ea9f27fe471cf6c18
RDCoder8 <mojinr@gmail.com>
2023-08-29 11:16:37 -0400

    "Tip Calculator express application created."

--- server.js
+++ server.js
@@ -10,6 +10,11 @@ app.get('/greeting/:name', (req, res) => {
   res.send("<h1>What's good " + req.params.name + "?</h1>");
 });
 
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+    res.send("<h1>Your tip is: " + (req.params.total * (req.params.tipPercentage/100))  + "</h1>")
+})
+
+
 app.listen(3000, ()=> {
     console.log("Listening on port 3000")
 })
